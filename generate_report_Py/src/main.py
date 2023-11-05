e113bb781d84a16bcfb45aa93321917a068b958a
jplazag <jplazag@unal.edu.co>
2023-11-05 01:54:53 -0500

    Report generation with python

---
+++ generate_report_Py/src/main.py
@@ -0,0 +1,47 @@
+from generate_html import generate_html
+from generate_pdf import generate_pdf
+from plotter import plotter
+
+
+characterized_pump = {
+    "motor_speed" : 0, 
+    "power" : 0, 
+    "parking_slot" : 0,
+    "test_number" : 0,
+    "service_order" : "", 
+    "date" : "", 
+    "delegate" : "", 
+    "model" : "",
+    "flow" : [], 
+    "pressure" : [], 
+    "velocity" : [], 
+    "elevation" : [], 
+    "pump_total" : [], 
+    "pump_power" : [], 
+    "pump_efficiency" : [],
+    "final_flow" : 0,
+    "final_head" : 0, 
+    "final_efficiency" : 0
+}
+
+
+characterized_pump["flow"] = [1,2,3,4,5,6,7,8,9,10]
+characterized_pump["pressure"] =  [55, 54.5, 54, 53, 52.5, 51.7, 50, 48.5, 46, 44]
+characterized_pump["velocity"] =  [1,2,3,4,5,6,7,8,9,10]
+characterized_pump["elevation"] =  [1,2,3,4,5,6,7,8,9,10]
+characterized_pump["pump_total"] =  [1,2,3,4,5,6,7,8,9,10]
+characterized_pump["pump_power"] =  [6,12,14,20,22,24,35,45,44,53]
+characterized_pump["pump_efficiency"] =  [0,10,20,20,25,30,40,70,60,50]
+
+index = characterized_pump["pump_efficiency"].index(max(characterized_pump["pump_efficiency"]))
+characterized_pump["final_flow"] =  characterized_pump["flow"][index]
+characterized_pump["final_head"] =  characterized_pump["pressure"][index]
+characterized_pump["final_efficiency"] =  characterized_pump["pump_efficiency"][index]
+
+plotter(characterized_pump["flow"], characterized_pump["pump_power"], "FlowVsPower.png","Flujo vs Potencia","Flujo (m^3/s)","Potencia (kW)")
+plotter(characterized_pump["flow"], characterized_pump["pressure"], "FlowVsHead.png","Flujo vs Cabeza","Flujo (m^3/s)","Cabeza (m)")
+plotter(characterized_pump["flow"], characterized_pump["pump_efficiency"], "FlowVsEfficiency.png","Flujo vs Eficiencia","Flujo (m^3/s)","Eficiencia (%)")
+
+generate_html(characterized_pump)
+
+generate_pdf(characterized_pump["test_number"])
---
+++ generate_report_Py/src/plotter.py
@@ -0,0 +1,27 @@
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plotter(x, y, file_name,title,x_label,y_label):
+    
+    coefficients = np.polyfit(x, y, 3)
+    curve = np.poly1d(coefficients)
+
+    # Generate more points on the curve for a smoother plot
+    x_curve = np.linspace(min(x), max(x), 100)
+    y_curve = curve(x_curve)
+
+
+
+    # Plot the interpolated curve
+    plt.figure(figsize=(15,5))
+    plt.grid(color='0.7', linestyle='--')
+    plt.plot(x_curve, y_curve, zorder=1)
+    plt.scatter(x, y, color='black', zorder=2)
+    plt.scatter(x_curve[np.where(y_curve == y_curve.max())], y_curve.max(), color='red', zorder=3)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    path_to_img = os.path.join("data", "imgs", file_name)
+    plt.savefig(path_to_img)
