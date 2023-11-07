cb49c5d90c26c355914d125e6dfd71c2da59ed61
jplazag <jplazag@unal.edu.co>
2023-11-07 12:23:00 -0500

    Starting with measurements

--- generate_report_Py/src/main.py
+++ generate_report_Py/src/main.py
@@ -33,15 +33,19 @@ characterized_pump["pump_total"] =  [1,2,3,4,5,6,7,8,9,10]
 characterized_pump["pump_power"] =  [6,12,14,20,22,24,35,45,44,53]
 characterized_pump["pump_efficiency"] =  [0,10,20,20,25,30,40,70,60,50]
 
-index = characterized_pump["pump_efficiency"].index(max(characterized_pump["pump_efficiency"]))
+# Finding most efficient point
+index = characterized_pump["pump_efficiency"].index(max(characterized_pump["pump_efficiency"])) 
 characterized_pump["final_flow"] =  characterized_pump["flow"][index]
 characterized_pump["final_head"] =  characterized_pump["pressure"][index]
 characterized_pump["final_efficiency"] =  characterized_pump["pump_efficiency"][index]
 
-plotter(characterized_pump["flow"], characterized_pump["pump_power"], "FlowVsPower.png","Flujo vs Potencia","Flujo (m³/s)","Potencia (kW)")
-plotter(characterized_pump["flow"], characterized_pump["pressure"], "FlowVsHead.png","Flujo vs Cabeza","Flujo (m³/s)","Cabeza (m)")
-plotter(characterized_pump["flow"], characterized_pump["pump_efficiency"], "FlowVsEfficiency.png","Flujo vs Eficiencia","Flujo (m³/s)","Eficiencia (%)")
+# Generate the graphs of power, pressure, efficiency vs flow
+plotter(characterized_pump["flow"], characterized_pump["pump_power"], "FlowVsPower.png","Flujo vs Potencia","Flujo (L/min)","Potencia (kW)")
+plotter(characterized_pump["flow"], characterized_pump["pressure"], "FlowVsHead.png","Flujo vs Cabeza","Flujo (L/min)","Cabeza (m)")
+plotter(characterized_pump["flow"], characterized_pump["pump_efficiency"], "FlowVsEfficiency.png","Flujo vs Eficiencia","Flujo (L/min)","Eficiencia (%)")
 
+# Generate a html file that is going to be used as a base for the pdf generation
 generate_html(characterized_pump)
 
+# Generate the final report in pdf
 generate_pdf(characterized_pump["test_number"])
--- generate_report_Py/src/user_interface_Qt.py
+++ generate_report_Py/src/user_interface_Qt.py
@@ -4,7 +4,37 @@ from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QFont
 
-title = QFont("Ubuntu", 20)
+from generate_html import generate_html
+from generate_pdf import generate_pdf
+from plotter import plotter
+
+import time
+
+characterized_pump = {
+    "pump_type": "",
+    "service_order" : "", 
+    "date" : "", 
+    "delegate" : "", 
+    "model" : "",
+
+    "motor_speed" : 0, 
+    "power" : 0, 
+    "parking_slot" : 0,
+    "test_number" : 0,
+    
+    "flow" : [], 
+    "pressure" : [], 
+    "velocity" : [], 
+    "elevation" : [], 
+    "pump_total" : [], 
+    "pump_power" : [], 
+    "pump_efficiency" : [],
+    "final_flow" : 0,
+    "final_head" : 0, 
+    "final_efficiency" : 0,
+    "total_measurements": 0,
+    "actual_measurement": 0
+}
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self, path, screen_width):
@@ -26,7 +56,7 @@ class Window(QtWidgets.QMainWindow):
         for i in range(main_object.layout().count()):
             element = main_object.layout().itemAt(i)
             if type(element) is QtWidgets.QWidgetItem :
-                if "label" in element.widget().objectName():
+                if "label" or "li_" in element.widget().objectName():
                     element.widget().setStyleSheet(f''' font-size: {text_font_size}px; ''')
 
                 if "title" in element.widget().objectName():
@@ -36,8 +66,7 @@ class Window(QtWidgets.QMainWindow):
                     element.widget().setStyleSheet(f''' font-size: {button_font_size}px; ''')
                 
                 if "alerts" in element.widget().objectName():
-                    element.widget().setStyleSheet(f''' font-size: {int(title_font_size* 3/2)}px; color: green''')
-                    # element.widget().setStyleSheet("color: green")
+                    element.widget().setStyleSheet(f''' font-size: {int(title_font_size * 3/2)}px; color: red''')
             self.defineFontSizes(element)
 
 
@@ -45,54 +74,109 @@ class Window(QtWidgets.QMainWindow):
 class FirstWindow(Window):
     def __init__(self, path, screen_width):
         super().__init__(path, screen_width)
-        self.pushButton.clicked.connect(self.goToNextScreen)
-
-        # self.order_service
-        # self.delegate
-        # self.date
-        # self.pump_model
-        # self.measurements
+        self.pushButton.clicked.connect(self.goToNextTask)
     
-    def goToNextScreen(self):
+    def goToNextTask(self):
         widget.setCurrentIndex(1)
 
-        # self.order_service = self.order_service.text()
-        # self.delegate = self.delegate.text()
-        # self.date = self.date.text()
-        # self.pump_model = self.pump_model.text()
-        # self.measurements = self.measurements.text()
+        if self.roto.isChecked():
+            characterized_pump["pump_type"] = "roto"
+        if self.triplex.isChecked():
+            characterized_pump["pump_type"] = "triplex" 
+
+        characterized_pump["service_order"] = self.lE_1_service_order.text()
+        characterized_pump["delegate"] = self.lE_2_delegate.text()
+        characterized_pump["date"] = self.lE_3_date.text()
+        characterized_pump["pump_model"] = self.lE_4_pump_model.text()
+        characterized_pump["total_measurements"] = self.measurements.text()
+
+        print(characterized_pump)
 
 class SecondWindow(Window):
     def __init__(self, path, screen_width):
         super().__init__(path, screen_width)
-        self.pushButton.clicked.connect(self.goToNextScreen)
+        self.pushButton.clicked.connect(self.goToNextTask)
     
-    def goToNextScreen(self):
-        widget.setCurrentIndex(0)
+    def goToNextTask(self):
+
+        self.takeMeasurement(characterized_pump["actual_measurement"])
+
+        characterized_pump["actual_measurement"] += 1
+
+        if characterized_pump["actual_measurement"] == characterized_pump["total_measurements"]:
+            widget.setCurrentIndex(2)
+
+    def checkStability(self):
+
+        self.alerts.setText("Midiendo estabilidad")
+        self.alerts.setStyleSheet(f''' color: green ''')
+
+        gain = 1
+        if characterized_pump["pump_type"] == "roto": #!Change pin numbers            
+            sensor_1_pin = 0
+            sensor_2_pin = 0
+
+        if characterized_pump["pump_type"] == "roto": #!Change pin numbers
+            sensor_1_pin = 0
+            sensor_2_pin = 0
+
+        average_m_1 = 0
+        average_m_2 = 0
+        counter = 0
+
+        while True:
+            pressure_1 = 0 # adc.read_adc(sensor_1_pin, gain=gain) * (4.096/32767)
+            pressure_2 = 0 # adc.read_adc(sensor_2_pin, gain=gain) * (4.096/32767)
+
+            average_m_1 += pressure_1
+            average_m_2 += pressure_2
+
+            counter += 1
+
+            # Search for unstable values
+            if abs(pressure_1 - average_m_1/counter) > 0.06 * average_m_1/counter or abs(pressure_2 - average_m_2/counter) > 0.06 * average_m_2/counter:
+                average_m_1 = 0
+                average_m_2 = 0
+                counter = 0
+
+            
+class ThirdWindow(Window):
+    def __init__(self, path, screen_width):
+        super().__init__(path, screen_width)
+        self.pushButton.clicked.connect(self.goToNextTask)
+    
+    def goToNextTask(self):
+
+        characterized_pump["flow"] = [1,2,3,4,5,6,7,8,9,10]
+        characterized_pump["pressure"] =  [55, 54.5, 54, 53, 52.5, 51.7, 50, 48.5, 46, 44]
+        characterized_pump["velocity"] =  [1,2,3,4,5,6,7,8,9,10]
+        characterized_pump["elevation"] =  [1,2,3,4,5,6,7,8,9,10]
+        characterized_pump["pump_total"] =  [1,2,3,4,5,6,7,8,9,10]
+        characterized_pump["pump_power"] =  [6,12,14,20,22,24,35,45,44,53]
+        characterized_pump["pump_efficiency"] =  [0,10,20,20,25,30,40,70,60,50]
+
+        # Finding most efficient point
+        index = characterized_pump["pump_efficiency"].index(max(characterized_pump["pump_efficiency"])) 
+        characterized_pump["final_flow"] =  characterized_pump["flow"][index]
+        characterized_pump["final_head"] =  characterized_pump["pressure"][index]
+        characterized_pump["final_efficiency"] =  characterized_pump["pump_efficiency"][index]
+
+        # Generate the graphs of power, pressure, efficiency vs flow
+        plotter(characterized_pump["flow"], characterized_pump["pump_power"], "FlowVsPower.png","Flujo vs Potencia","Flujo (L/min)","Potencia (kW)")
+        plotter(characterized_pump["flow"], characterized_pump["pressure"], "FlowVsHead.png","Flujo vs Cabeza","Flujo (L/min)","Cabeza (m)")
+        plotter(characterized_pump["flow"], characterized_pump["pump_efficiency"], "FlowVsEfficiency.png","Flujo vs Eficiencia","Flujo (L/min)","Eficiencia (%)")
+
+        # Generate a html file that is going to be used as a base for the pdf generation
+        generate_html(characterized_pump)
+
+        # Generate the final report in pdf
+        generate_pdf(characterized_pump["test_number"])
+        
         
 
 
 
-characterized_pump = {
-    "motor_speed" : 0, 
-    "power" : 0, 
-    "parking_slot" : 0,
-    "test_number" : 0,
-    "service_order" : "", 
-    "date" : "", 
-    "delegate" : "", 
-    "model" : "",
-    "flow" : [], 
-    "pressure" : [], 
-    "velocity" : [], 
-    "elevation" : [], 
-    "pump_total" : [], 
-    "pump_power" : [], 
-    "pump_efficiency" : [],
-    "final_flow" : 0,
-    "final_head" : 0, 
-    "final_efficiency" : 0
-}
+
 
 
 app = QtWidgets.QApplication(sys.argv)
