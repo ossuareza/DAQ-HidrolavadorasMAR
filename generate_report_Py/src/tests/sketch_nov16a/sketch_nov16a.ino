42e93a364ca419a14c85d4138d47782736fd1d46
jplazag <jplazag@unal.edu.co>
2023-11-17 23:25:43 -0500

    Tests on Hidrolavadoras Mar V.1

--- generate_report_Py/src/tests/sketch_nov16a/sketch_nov16a.ino
+++ generate_report_Py/src/tests/sketch_nov16a/sketch_nov16a.ino
@@ -9,21 +9,22 @@ void loop() {
 
   v_pressure_1 = -1;
   v_pressure_2 = -1;
-
-  if (Serial.available() > 0) {
+//Serial.available() > 0
+  if (true) {
     String data = Serial.readStringUntil('\n');
     if (data == "R") {                   // Rotodinámicas
       v_pressure_1 = analogRead(A0);
-      v_pressure_2 = analogRead(A1);
+      v_pressure_2 = analogRead(A3);
     }
     else if (data == "T"){               //Triplex
-      v_pressure_1 = analogRead(A2);
-      v_pressure_2 = analogRead(A3);
+      v_pressure_1 = analogRead(A6);
+      v_pressure_2 = analogRead(A9);
     }
   
 //    Serial.print("You sent me: ");
+    v_pressure_1 = analogRead(A0);
     Serial.println(v_pressure_1);
-    Serial.println(v_pressure_2);
+//    Serial.println(v_pressure_2);
 //    Serial.println(data);
 //    delay(1000);
   }
